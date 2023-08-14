e2e15f3472bc89a75e9449a5d6f42d24efed27af
emma.galliere <emma.galliere@artefact.com>
2023-08-14 11:29:20 +0200

    Add log streaming and --manifest option

--- cli/cli.py
+++ cli/cli.py
@@ -3,8 +3,10 @@ import requests
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import json
+import time
 
-from utils import load_file, extract_manifest_filename_from_command
+from utils import load_file
 
 dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
@@ -16,25 +18,62 @@ DBT_PROJECT_FILE = os.getenv('DBT_PROJECT_FILE')
 
 @click.command(help='Enter dbt command, ex: dbt-remote run --select test')
 @click.argument('dbt_command')
-def cli(dbt_command: str):
+@click.option('--manifest', '-m', default='./target/manifest.json', help='Manifest file, ex: ./target/manifest.json')
+def cli(dbt_command: str, manifest: str):
     click.echo('Command: {0}'.format(dbt_command))
-    res = send_command(dbt_command)
-    click.echo(res)
+    server_res = send_command(dbt_command, manifest)
+
+    uuid = json.loads(server_res)['uuid']
+    click.echo("uuid: {0}".format(uuid))
+
+    stream_log(uuid)
 
 
-def send_command(command: str):
+def send_command(command: str, manifest: str):
     url = SERVER_URL + "dbt"
 
-    manifest_filename, processed_command = extract_manifest_filename_from_command(command, MANIFEST_FILENAME)
-
-    manifest_str = load_file(manifest_filename)
+    manifest_str = load_file(manifest)
     dbt_project_str = load_file(DBT_PROJECT_FILE)
 
     data = {
-            "command": processed_command,
+            "command": command,
             "manifest": manifest_str,
             "dbt_project": dbt_project_str
         }
 
     res = requests.post(url=url, json=data)
     return res.text
+
+
+def stream_log(uuid: str):
+    click.echo("Waiting for job execution...")
+    time.sleep(16)
+    run_status = json.loads(get_run_status(uuid))["run_status"]
+    last_logs = []
+
+    while run_status == "running":
+        time.sleep(1)
+        run_status_json = json.loads(get_run_status(uuid))
+        run_status = run_status_json["run_status"]
+        last_logs = show_last_logs(uuid, last_logs)
+
+    while "END JOB" not in last_logs[-1]:
+        time.sleep(1)
+        last_logs = show_last_logs(uuid, last_logs)
+
+
+def get_run_status(uuid: str):
+    url = SERVER_URL + "job/" + uuid
+    res = requests.get(url=url)
+    return res.text
+
+
+def show_last_logs(uuid: str, last_logs=[]):
+    run_status_json = json.loads(get_run_status(uuid))
+    logs = run_status_json["entries"]  # gets 5 last logs from Firestore
+    for log in logs:
+        if log not in last_logs:
+            print(log)
+            last_logs.append(log)
+    last_logs = last_logs[-5:]
+    return last_logs
--- cli/utils.py
+++ cli/utils.py
@@ -1,23 +1,4 @@
-import re
-
-
 def load_file(filename):
-    f = open(filename, 'r')
-    file_str = f.read()
-    f.close()
+    with open(filename, 'r') as f:
+        file_str = f.read()
     return file_str
-
-
-def extract_manifest_filename_from_command(command: str, MANIFEST_FILENAME: str):
-    processed_command = command
-    m = re.search('--manifest (.+?)( |$)', command)
-    if m:
-        manifest_filename = m.group(1)
-        begin, end = m.span()
-        if processed_command[end:] != "":
-            processed_command = processed_command[:begin] + processed_command[end:]
-        else:
-            processed_command = processed_command[:begin-1]
-    else:
-        manifest_filename = MANIFEST_FILENAME
-    return manifest_filename, processed_command
