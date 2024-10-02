52e4c868a0e301732abf75133d9edcc20e454dc2
Mariam <mariam.ahhttouche@artefact.com>
2024-10-02 10:58:13 +0200

    clean lambda function code

--- awspy/lambda/dbtauth.py
+++ awspy/lambda/dbtauth.py
@@ -1,8 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    print("event ", event)
-    print("context ", context)
     return {
         'statusCode': 200,
         'body': json.dumps('user authenticated')
